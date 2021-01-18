78230c71d7c93e7c392df174059734d7d58c4b0d
lstaborda <leticia.s.taborda@gmail.com>
2021-01-17 22:41:42 -0300

    Create routes to GET and POST transactions with categories

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,18 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const income = transactions.reduce((prev, current) => {
+      return current.type === 'income' ? prev + current.value : prev;
+    }, 0);
+
+    const outcome = transactions.reduce((prev, current) => {
+      return current.type === 'outcome' ? prev + current.value : prev;
+    }, 0);
+
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,61 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const balance = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError('The outcome value is greater than the total');
+    }
+
+    let categoryValue: Category;
+
+    const checkCategoryTitleExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (checkCategoryTitleExists) {
+      categoryValue = checkCategoryTitleExists;
+    } else {
+      categoryValue = categoryRepository.create({ title: category });
+      await categoryRepository.save(categoryValue);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryValue.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    const createdTransaction = {
+      ...transaction,
+      category: categoryValue,
+    };
+
+    return createdTransaction;
   }
 }
 
